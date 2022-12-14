ecbe25100691ac5c66ad85b8c83c7092f077fb34
DilanNestaresRamos <dnestaresramos@gmail.com>
2022-12-13 23:36:46 -0500

    EurekaServer

---
+++ src/main/java/com/ef/dilannestares/EurekaServer/EfDilanNestaresRamosEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ef.dilannestares.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfDilanNestaresRamosEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfDilanNestaresRamosEurekaServerApplication.class, args);
+	}
+
+}
